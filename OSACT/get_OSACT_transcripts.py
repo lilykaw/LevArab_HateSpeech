a159c0d1b0f5b5a10f2431a82c1f3a05c4dac4a4
lilykaw <lily.kawaoto@gmail.com>
2022-11-26 13:49:11 -0500

    fixed more bugs, created combined train set

--- OSACT/get_OSACT_transcripts.py
+++ OSACT/get_OSACT_transcripts.py
@@ -2,6 +2,7 @@ from dataclasses import replace
 import re
 import csv
 import random
+import time
 from googletrans import Translator # pip install googletrans==3.1.0a0
 from emot.emo_unicode import UNICODE_EMOJI, EMOTICONS_EMO, UNICODE_EMOJI_ALIAS
 
@@ -53,8 +54,10 @@ def preprocess(txt):
     patterns = ['#', '@', 'USER', ':', ';', 'RT', 'URL', '<LF>', '\.\.\.', '…', '!', '\.', '\?', '%', '\*', '"', "'", '\$', '\&', '/', '\)', '\(', '\[', '\]', '\}', '\{', '|', '\d+']
     text = re.sub('|'.join(patterns), '', txt)          # remove patterns
     text = re.sub(r'[a-zA-Z]', '', text)                # remove non-Arabic characters
+    text = re.sub(r'\t', ' ', text)                     # replace tabs with single space
     text = re.sub(r'(.)\1\1+', r'\1', text)             # remove 3 or more repetitions of any character
     text = emoji_to_text(text)                          # replace emojis with their Arabic description
+    time.sleep(0.5)
     text = emoticon_to_text(text)                       # replace emoticons with their Arabic description
     return text
 
@@ -65,18 +68,42 @@ Store cleaned text plus its label in a tsv file.
 
 """ TRAINING FILE"""
 with open(OSACT_XYTRAIN_PATH, 'r') as f:
+    osact_train_list = []
     reader = csv.reader(f, delimiter=",")
-    osact_train_list = [(preprocess(row[0]), row[1]) for row in reader]
-with open('osact_train_cleaned2.tsv', 'w') as f:
+    for i,row in enumerate(reader): 
+        row = [nonspace for nonspace in row if nonspace]
+        
+        # For some reason, some of these rows result in either 1 or 3 columns. To be investigated further.
+        if (len(row) != 2):
+            if len(row)==1:
+                row = row[0].split('\t')
+                print(f"New row: {row}\n")
+                # TO-DO: find where row becomes only ['R']  
+                # assert(len(row)==3)
+                if len(row)!=3:
+                    continue
+                osact_train_list.append((preprocess(row[0]), row[2]))
+            elif len(row)==3:
+                row = ' '.join(row)
+                row = row[0].split('\t')
+                print(f"New row: {row}\n")
+                # assert(len(row)==3)
+                if len(row)!=3:
+                    continue
+                osact_train_list.append((preprocess(row[0]), row[2]))
+        
+        else:
+            osact_train_list.append((preprocess(row[0]), row[1].strip()))
+with open('osact_train_cleaned4.tsv', 'w') as f:
     for pair in osact_train_list:
         f.write(f"{pair[0]}\t{pair[1]}\n")
 
 
 """ TEST FILES (TXT & LABELS) """
-with open(OSACT_XTEST_PATH, 'r') as f1, open(OSACT_YTEST_PATH, 'r') as f2:
-    reader1 = f1.read().splitlines() 
-    reader2 = f2.read().splitlines()
-    osact_test_list = [(preprocess(row), reader2[i]) for i,row in enumerate(reader1)]  
-with open('osact_test_cleaned2.tsv', 'w') as f:
-    for pair in osact_test_list:
-        f.write(f"{pair[0]}\t{pair[1]}\n")
+# with open(OSACT_XTEST_PATH, 'r') as f1, open(OSACT_YTEST_PATH, 'r') as f2:
+#     reader1 = f1.read().splitlines() 
+#     reader2 = f2.read().splitlines()
+#     osact_test_list = [(preprocess(row), reader2[i]) for i,row in enumerate(reader1)]  
+# with open('osact_test_cleaned3.tsv', 'w') as f:
+#     for pair in osact_test_list:
+#         f.write(f"{pair[0]}\t{pair[1]}\n")
--- get_combined_tsv.py
+++ get_combined_tsv.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
+COMBINED_TSV_PATH = 'combined_train.tsv'
 
-df1 = pd.read_csv('/Users/lilykawaoto/Documents/GitHub/LING-L715/lhsab_train.tsv', sep="\t")              # LHSAB train
-df2 = pd.read_csv('/Users/lilykawaoto/Documents/GitHub/LING-L715/osact_train_cleaned.tsv', sep="\t")      # OSACT train
-newdf = df1.merge(df2, how='outer')
-newdf.to_csv('/Users/lilykawaoto/Documents/GitHub/LING-L715/combined_train.tsv', sep="\t")
+### Read in LHSAB and OSACT datasets
+df1 = pd.read_csv('/Users/lilykawaoto/Documents/GitHub/LING-L715/lhsab_train.tsv', sep="\t", header=None)             # LHSAB train
+df2 = pd.read_csv('/Users/lilykawaoto/Documents/GitHub/LING-L715/osact_train_cleaned.tsv', sep="\t", error_bad_lines=False, header=None)     # OSACT train
+newdf = df1.append(df2, ignore_index=True)
+
+### Standardize label column (for LHSAB dataset: "hate"->"HS", "normal"/"abusive"->"NOT_HS")
+newdf[1] = newdf[1].str.replace('hate', 'HS')
+newdf[1] = newdf[1].str.replace('normal', 'NOT_HS')
+newdf[1] = newdf[1].str.replace('abusive', 'NOT_HS')
+
+### Save as new tsv file
+newdf.to_csv('combined_train.tsv', sep='\t', header=False, index=False)
